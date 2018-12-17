41f42d69a424f824fd8498a652d5dac6540ba19a
admin@hinotori.hiroshima-u.ac.jp <admin@hinotori.hiroshima-u.ac.jp@huc12-hosting-web01.hiroshima-u.ac.jp>
2018-12-18 01:56:30 +0900

    fix indentation

--- toolkit.js
+++ toolkit.js
@@ -64,12 +64,12 @@ $(document).on('click', 'td', function() {
 		i;
 
 		for (i = 0; i < sURLVariables.length; i++) {
-		sParameterName = sURLVariables[i].split('=');
+			sParameterName = sURLVariables[i].split('=');
 
-		if (sParameterName[0] === sParam) {
-		return sParameterName[1] === undefined ? true : sParameterName[1];
-		}
-		}
+				if (sParameterName[0] === sParam) {
+					return sParameterName[1] === undefined ? true : sParameterName[1];
+				}
+			}
 		};
 
 		var tr = $(this).closest('tr');
